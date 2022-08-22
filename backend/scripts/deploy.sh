35b4a6d61dbe66a267eb46110ffea473df7a67f4
domo7304 <domo7304@naver.com>
2022-08-23 01:21:24 +0900

    fix: deploy.sh 프로세스 pid 확인 코드 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f backend)
+CURRENT_PID=$(pgrep -f java)
 
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
